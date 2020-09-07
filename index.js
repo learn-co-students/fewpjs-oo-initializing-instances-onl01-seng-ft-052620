7176e6ae4d33c6e2a1e87559bb32e9ace330dc7a
lovecosma <williamsmatthew333@gmail.com>
2020-09-06 21:15:12 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here
+class Breakfast {
+  constructor(string1, string2) {
+  this.food = string1
+  this.drink = string2
+  }
+}
+
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad
+    this.drink = drink
+    this.soup = soup
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, _dessert) {
+    this.soup = soup
+    this.salad = salad
+    this.entree = entree
+    this._dessert = _dessert
+  }
+}
