b453d3a06ff6a585f445c41c7c1cee9af9ccd682
SWFullPotential <swfullpotential@gmail.com>
2020-09-09 11:57:34 -0600

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// //class Dog {
-//     constructor(name, breed) {
-//         this.name = name;
-//         this.breed = breed;
-//       }
-//     }
-
 class Breakfast {
     constructor(food, drink){
         this.food = food;
